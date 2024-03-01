b0990d2807123a875e706d3e4a64ecaccf2ea93a
Kathrin Lembcke <kathrin.l78@gmx.de>
2024-03-01 23:40:16 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function saveLead() {
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+})
+
+for(let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
